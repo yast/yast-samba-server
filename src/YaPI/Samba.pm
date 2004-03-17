92cc0d5717cbac9ddd35a7c54c6ed6f01961af6b
Stanislav Višňovský <visnov@suse.cz>
2004-03-17 17:02:39 +0000

    API implemented, a lot of testing needed
    svn path=/trunk/samba-server/; revision=16515

--- src/YaPI/Samba.pm
+++ src/YaPI/Samba.pm
@@ -22,22 +22,22 @@ $enabled = GetServiceStatus()
   returns true if the services smbd and nmbd are enabled, false if either of
   them is disabled or undef on failure.
 
-$enabled = ModifyService($enable)
+$enabled = EditService($enable)
 
   enabled/disables the smbd and nmbd services. returns the status of the
   service, undef on failure.
 
-$result = ModifyServerAsBDC($pdc_name)
+$result = EditServerAsBDC($pdc_name)
 
   configures the samba server as a Backup Domain Controller with the given
   PDC. return undef on failure.
 
-$result = ModifyServerAsPDC()
+$result = EditServerAsPDC()
 
   configures the samba server as a Backup Domain Controller with the given
   PDC. return undef on failure.
 
-$result = ModifyServerAsStandalone()
+$result = EditServerAsStandalone()
 
   configures the samba server as a Backup Domain Controller with the given
   PDC. return undef on failure.
@@ -47,46 +47,45 @@ $description = GetServerDescription()
   returns the server comment string, or undef it is not configured or on
   failure.
 
-$result = ModifyServerDescription($description)
+$result = EditServerDescription($description)
 
   sets a new server comment string. returns undef on failure
 
-$passdbHash = GetSAMConfiguration($samString)
+@passdb = GetSAMBackends()
 
-  returns a hash containing the configuration options related to a passdb
-  backend. return undef on failure.
+  returns a list of all configured passdb backends or undef on failure.
 
-$result = ModifySAMConfiguration($samString,$passdbHash)
+$result = EditSAMConfiguration($samString,$passdbHash)
 
   sets the options for a passdb backend. returns undef on failure.
 
-$result = SetDefaultSAM($samString)
+$result = EditDefaultSAM($samString)
 
   sets the given passdb backend as default (used for creating new users).
   returns undef on error.
 
-$result = CreateSAMConfiguration($samString,$passdbHash)
+$result = AddSAM($samString,$isDefault)
 
-  creates a new passdb backend using the given options. returns undef
+  creates a new passdb backend. returns undef
   on error.
 
-$result = RemoveSAM($samString)
+$result = DeleteSAM($samString)
 
-  removes the given passdb backend. returns undef on failure.
+  Deletes the given passdb backend. returns undef on failure.
 
 $result = EnableShare($shareName,$enable)
 
   enables/disabled the given share. returns undef on failure.
 
-$result = CreateShare($shareName,$options)
+$result = AddShare($shareName,$options)
 
   creates a new share using the given options. returns undef on failure.
 
-$result = RemoveShare($shareName)
+$result = DeleteShare($shareName)
 
-  removes a share completely. returns undef on failure.
+  Deletes a share completely. returns undef on failure.
 
-$result = ModifyShare($shareName,$options)
+$result = EditShare($shareName,$options)
 
   modifies an existing share using the given options. returns undef on failure.
 
@@ -126,6 +125,11 @@ package YaPI::Samba;
 use YaST::YCP;
 BEGIN { push( @INC, '/usr/share/YaST2/modules/' ); }
 
+YaST::YCP::Import ("SambaServer");
+YaST::YCP::Import ("SambaServerPassdb");
+
+our %TYPEINFO;
+
 if(not defined do("YaPI.inc")) {
     die "'$!' Can not include YaPI.inc";
 }
@@ -150,9 +154,8 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{GetServiceStatus} = ["function", "string" ]; }
 sub GetServiceStatus {
-    # TODO:
-    $self = shift;    
-    return undef;
+    my $self = shift;    
+    return YaST::Service::Enabled ("smbd") && YaST::Service::Enabled ("nmbd");
 }
 
 =item *
@@ -167,13 +170,13 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{DetermineRole} = ["function", "string" ]; }
 sub DetermineRole {
-    # TODO:
-    $self = shift;    
-    return undef;
+    my $self = shift;
+    SambaServer::Read ();
+    return SambaServer::DetermineRole ();
 }
 
 =item *
-C<$enabled = ModifyService($enable);>
+C<$enabled = EditService($enable);>
 
 Modifies the status of the service. If the parameter is true, 
 smb and nmb services are enabled in the default runlevels, 
@@ -184,16 +187,26 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyService} = ["function", "boolean", "boolean" ]; }
-sub ModifyService {
-    # TODO:
-    $self = shift;    
-    $enable = shift;
-    return undef;
+BEGIN { $TYPEINFO{EditService} = ["function", "boolean", "boolean" ]; }
+sub EditService {
+    my $self = shift;    
+    my $enable = shift;
+
+    unless (YaST::Service::Enable ("smbd", $enable))
+    {
+	return undef;
+    }
+
+    unless (YaST::Service::Enable ("nmbd", $enable))
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = ModifyServerAsBDC($pdc_name)>
+C<$result = EditServerAsBDC($pdc_name)>
 
 Configures the global settings of a server to behave like a 
 backup domain controller. The primary domain controller is setup by the argument.
@@ -202,16 +215,28 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyServerAsBDC} = ["function", "void", "string" ]; }
-sub ModifyServerAsBDC {
-    # TODO:
-    $self = shift;   
-    $pdc = shift; 
-    return undef;
+BEGIN { $TYPEINFO{EditServerAsBDC} = ["function", "void", "string" ]; }
+sub EditServerAsBDC {
+    my $self = shift;   
+    my $pdc = shift; 
+    
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    SambaServer::SetAsBDC ($pdc);
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = ModifyServerAsPDC()>
+C<$result = EditServerAsPDC()>
 
 Configures the global settings of a server to behave like a primary domain controller.
 On error, undef is returned and the Error() function can be used
@@ -219,15 +244,27 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyServerAsPDC} = ["function", "void" ]; }
-sub ModifyServerAsPDC {
-    # TODO:
-    $self = shift;
-    return undef;
+BEGIN { $TYPEINFO{EditServerAsPDC} = ["function", "void" ]; }
+sub EditServerAsPDC {
+    my $self = shift;
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    SambaServer::SetAsPDC ();
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = ModifyServerAsStandalone();>
+C<$result = EditServerAsStandalone();>
 
 Configures the global settings of a server to behave like a standalone 
 server not taking part in any domain.
@@ -236,11 +273,23 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyServerAsStandalone} = ["function", "void" ]; }
-sub ModifyServerAsStandalone {
-    # TODO:
-    $self = shift;    
-    return undef;
+BEGIN { $TYPEINFO{EditServerAsStandalone} = ["function", "void" ]; }
+sub EditServerAsStandalone {
+    my $self = shift;    
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    SambaServer::SetAsPDC ();
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
@@ -254,13 +303,18 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{GetServerDescription} = ["function", "string" ]; }
 sub GetServerDescription {
-    # TODO:
-    $self = shift;    
-    return undef;
+    my $self = shift;    
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    return SambaServer::server_string ();
 }
 
 =item *
-C<$result = ModifyServerDescription($description);>
+C<$result = EditServerDescription($description);>
 
 Configures the description of the server shown in clients. 
 On error, undef is returned and the Error() function can be used
@@ -268,16 +322,20 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyServerDescription} = ["function", "void", "string" ]; }
-sub ModifyServerDescription {
-    # TODO:
-    $self = shift;    
-    $description = shift;
-    return undef;
+BEGIN { $TYPEINFO{EditServerDescription} = ["function", "void", "string" ]; }
+sub EditServerDescription {
+    my $self = shift;    
+    my $description = shift;
+
+    SambaServer::Read ();
+    SambaServer::setDescription ($description);
+    SambaServer::Write ();
+
+    return 1;
 }
 
 =item *
-C<$passdbHash = GetSAMConfiguration($samString);>
+C<@passdb = GetSAMBackends();>
 
 Returns a list of options specified for the given SAM. The structure of the options is sam-type specific.
 On error, undef is returned and the Error() function can be used
@@ -285,17 +343,20 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{GetSAMConfiguration} = ["function", [ "map", "string", "any" ], "string" ]; }
+BEGIN { $TYPEINFO{GetSAMBackends} = ["function", [ "list", "string" ] ]; }
 sub GetSAMConfiguration {
-    # TODO
-    $self = shift;
-    $sam = shift;
+    my $self = shift;
     
-    return undef;
+    unless (SambaServerPassdb::Read ())
+    {
+	return undef;
+    }
+    
+    return SambaServerPassdb::GetBackends ();
 }
 
 =item *
-C<$result = ModifySAMConfiguration($samString, $passdbHash);>
+C<$result = EditSAMConfiguration($samString, $passdbHash);>
 
 Modifies the configuration of the given sam. The structure of the hash 
 must follow the structure as specified for GetSAMConfiguration. 
@@ -304,18 +365,32 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifySAMConfiguration} = ["function", "boolean", "string", ["map", "string", "any" ] ]; }
-sub ModifySAMConfiguration {
-    # TODO
-    $self = shift;
-    $sam = shift;
-    %options = shift;
+BEGIN { $TYPEINFO{EditSAMConfiguration} = ["function", "boolean", "string", ["map", "string", "any" ] ]; }
+sub EditSAMConfiguration {
+    my $self = shift;
+    my $sam = shift;
+    my %options = shift;
     
-    return undef;
+    unless (SambaServerPassdb::Read ())
+    {
+	return undef;
+    }
+    
+    unless (SambaServerPassdb::EditSAMConfiguration ($sam, %options))
+    {
+	return undef;
+    }
+
+    unless (SambaServerPassdb::Write ())
+    {
+	return undef;
+    }
+    
+    return 1;
 }
 
 =item *
-C<$result = SetDefaultSAM($samString);>
+C<$result = EditDefaultSAM($samString);>
 
 Sets the SAM as default one, meaning that adding a new user will be done using this SAM. 
 On error, undef is returned and the Error() function can be used
@@ -323,17 +398,32 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{SetDefaultSAM} = ["function", "boolean", "string" ]; }
-sub SetDefaultSAM {
-    # TODO
-    $self = shift;
-    $sam = shift;
+BEGIN { $TYPEINFO{EditDefaultSAM} = ["function", "boolean", "string" ]; }
+sub EditDefaultSAM {
+    my $self = shift;
+    my $sam = shift;
     
-    return undef;
+
+    unless (SambaServerPassdb::Read ())
+    {
+	return undef;
+    }
+    
+    unless (SambaServerPassdb::SetDefaultSAM ($sam))
+    {
+	return undef;
+    }
+
+    unless (SambaServerPassdb::Write ())
+    {
+	return undef;
+    }
+    
+    return 1;
 }
 
 =item *
-C<$result = CreateSAMConfiguration($samString, $passdbHash);>
+C<$result = AddSAM($samString,$isDefault);>
 
 Creates a new SAM using the given name and configuration. 
 The structure of the hash must follow the structure as specified for GetSAMConfiguration.
@@ -342,33 +432,79 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{CreateSAMConfiguration} = ["function", "boolean", "string", [ "map", "string", "any" ] , "boolean" ]; }
-sub CreateSAMConfiguration {
-    # TODO
-    $self = shift;
-    $sam = shift;
-    %options = shift;
-    $default = shift;
+BEGIN { $TYPEINFO{AddSAM} = ["function", "boolean", "string", "boolean" ]; }
+sub AddSAM {
+    my $self = shift;
+    my $sam = shift;
+    my $default = shift;
     
-    return undef;
+    my @current = $self->GetSAMBackends ();
+    
+    if( grep( /^$sam$/, @current ) ) {
+        # already there, error
+	return undef;
+    }
+    
+    if( $default )
+    {
+	unshift @current, $sam;
+    }
+    else
+    {
+	push @current, $sam;
+    }
+    
+    SambaServerPassdb::SetBackends (@current);
+    
+    unless (SambaServerPassdb::Write ())
+    {
+	return undef;
+    }
+    
+    return 1;
 }
 
 =item *
-C<$result = RemoveSAM($samString);>
+C<$result = DeleteSAM($samString);>
 
-Removes the specified sam. It is not possible to remove the default one.
+Deletes the specified sam. It is not possible to Delete the default one.
 On error, undef is returned and the Error() function can be used
 to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{RemoveSAM} = ["function", "boolean", "string" ]; }
-sub RemoveSAM {
-    # TODO
-    $self = shift;
-    $sam = shift;
+BEGIN { $TYPEINFO{DeleteSAM} = ["function", "boolean", "string" ]; }
+sub DeleteSAM {
+    my $self = shift;
+    my $sam = shift;
     
-    return undef;
+    my @current = $self->GetSAMBackends ();
+    
+    unless( grep( /^$sam$/, @current ) ) {
+        # not there, error
+	return undef;
+    }
+
+    # filter out the sam
+    my $item = undef;
+    my @new = ();
+    while (@current)
+    {
+	$item = shift @current;
+	if( $item != $sam )
+	{
+	    push @new, $item;
+	}
+    }
+    
+    SambaServerPassdb::SetBackends (@new);
+    
+    unless (SambaServerPassdb::Write ())
+    {
+	return undef;
+    }
+    
+    return 1;
 }
 
 =item *
@@ -382,16 +518,30 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{EnableShare} = ["function", "boolean", "string", "boolean"] ; }
 sub EnableShare {
-    # TODO
-    $self = shift;
-    $name = shift;
-    $on = shift;
-    
-    return undef;
+    my $self = shift;
+    my $name = shift;
+    my $on = shift;
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    unless (SambaServer::EnableShare ($name, Boolean($on) ))
+    {
+	return undef;
+    }
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = CreateShare($shareName,$options);>
+C<$result = AddShare($shareName,$options);>
 
 Creates a new share with the given name and initial options. 
 On error, undef is returned and the Error() function can be used
@@ -399,36 +549,64 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{CreateShare} = ["function", "boolean", "string", [ "map", "string", "any" ] ] ; }
-sub CreateShare {
-    # TODO
-    $self = shift;
-    $name = shift;
-    %options = shift;
+BEGIN { $TYPEINFO{AddShare} = ["function", "boolean", "string", [ "map", "string", "any" ] ] ; }
+sub AddShare {
+    my $self = shift;
+    my $name = shift;
+    my $options = shift;
     
-    return undef;
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    unless (SambaServer::AddShare ($name, $options ))
+    {
+	return undef;
+    }
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = RemoveShare($shareName);>
+C<$result = DeleteShare($shareName);>
 
-Removes the given share. 
+Deletes the given share. 
 On error, undef is returned and the Error() function can be used
 to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{RemoveShare} = ["function", "boolean", "string"] ; }
-sub RemoveShare {
-    # TODO
-    $self = shift;
-    $name = shift;
+BEGIN { $TYPEINFO{DeleteShare} = ["function", "boolean", "string"] ; }
+sub DeleteShare {
+    my $self = shift;
+    my $name = shift;
     
-    return undef;
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    unless (SambaServer::DeleteShare ($name))
+    {
+	return undef;
+    }
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
-C<$result = ModifyShare($shareName,$options);>
+C<$result = EditShare($shareName,$options);>
 
 Modifies the given share to use the given options. 
 On error, undef is returned and the Error() function can be used
@@ -436,14 +614,29 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyShare} = ["function", "boolean", "string", [ "map", "string", "any" ] ] ; }
-sub ModifyShare {
-    # TODO
-    $self = shift;
-    $name = shift;
-    %options = shift;
+BEGIN { $TYPEINFO{EditShare} = ["function", "boolean", "string", [ "map", "string", "any" ] ] ; }
+sub EditShare {
+
+    my $self = shift;
+    my $name = shift;
+    my $options = shift;
     
-    return undef;
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    unless (SambaServer::EditShare ($name, $options ))
+    {
+	return undef;
+    }
+
+    unless ( SambaServer::Write () )
+    {
+	return undef;
+    }
+
+    return 1;
 }
 
 =item *
@@ -455,14 +648,17 @@ to get the error hash.
 
 =cut
 
-BEGIN { $TYPEINFO{ModifyShare} = ["function", "boolean", "string", [ "map", "string", "any" ] ] ; }
-sub ModifyShare {
-    # TODO
-    $self = shift;
-    $name = shift;
-    %options = shift;
-    
-    return undef;
+BEGIN { $TYPEINFO{GetShare} = ["function", [ "map", "string", "any" ], "string" ] ; }
+sub GetShare {
+    my $self = shift;
+    my $name = shift;
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    return SambaServer::GetShare ($name);
 }
 
 =item *
@@ -477,10 +673,27 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{GetAllDirectories} = ["function", [ "list", "string" ] ]; } 
 sub GetAllDirectories {
-    # TODO
-    $self = shift;
+
+    my $self = shift;
+
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    my @res = ();
+    my %shares = SambaServer::shares ();
     
-    return undef;
+    # filter out the printers
+    while (my ($key, %vals) = each (%shares) )
+    {
+	unless (defined $vals {"printable"})
+	{
+	    push @res, $key;
+	}
+    }
+    
+    return \@res;
 }
 
 =item *
@@ -495,11 +708,10 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{EnableHomes} = ["function", "void",  "boolean"] ; }
 sub EnableHomes {
-    # TODO
-    $self = shift;
-    $on = shift;
+    my $self = shift;
+    my $on = shift;
     
-    return undef;
+    return $self->EnableShare ("homes", $on);
 }
 
 =item *
@@ -514,11 +726,10 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{EnableNetlogon} = ["function", "void",  "boolean"] ; }
 sub EnableNetlogon {
-    # TODO
-    $self = shift;
-    $on = shift;
+    my $self = shift;
+    my $on = shift;
     
-    return undef;
+    return $self->EnableShare ("netlogon", $on);
 }
 
 =item *
@@ -532,10 +743,26 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{GetAllPrinters} = ["function", [ "list", "string" ] ]; }
 sub GetAllPrinters {
-    # TODO
-    $self = shift;
+    my $self = shift;
     
-    return undef;
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    my @res = ();
+    my %shares = SambaServer::shares ();
+    
+    # filter out the printers
+    while (my ($key, %vals) = each (%shares) )
+    {
+	if (defined $vals {"printable"})
+	{
+	    push @res, $key;
+	}
+    }
+    
+    return \@res;
 }
 
 =item *
@@ -549,12 +776,33 @@ to get the error hash.
 
 BEGIN { $TYPEINFO{EnablePrinters} = ["function", "boolean", [ "list", "string" ], "boolean"] ; }
 sub EnablePrinters {
-    # TODO
-    $self = shift;
-    @printer_names = shift;
-    $enable = shift;
+    my $self = shift;
+    my @printer_names = shift;
+    my $enable = shift;
     
-    return undef;
+    unless (SambaServer::Read ())
+    {
+	return undef;
+    }
+
+    my %shares = SambaServer::shares ();
+    
+    while (my $share = pop @printer_names)
+    {
+	my %conf = $shares{ $share };
+	
+	unless (defined $conf {"printable"})
+	{
+	    return undef;
+	}
+	
+	$conf {"enabled"} = $enable;
+
+	$self->EditShare ($share, %conf);
+    }
+    
+    return 1;
+        
 }
 
 #######################################################
